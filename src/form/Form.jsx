a135f6395c0ad12535e39c61b46eaa45f902d36c
Arpita Behura <arpitabehura.009@gmail.com>
2024-03-26 19:04:40 +0100

    Converting form to multi-step form

--- src/form/Form.jsx
+++ src/form/Form.jsx
@@ -1,4 +1,4 @@
-import {useContext, useRef} from "react";
+import {useContext, useRef, useState} from "react";
 import {validateEmail, validatePassword} from "../helper/validation";
 import './Form.css';
 import Input from "../components/input/Input.jsx";
@@ -9,6 +9,10 @@ export default function Form(){
 
     const emailRef = useRef(null);
     const passwordRef = useRef(null);
+    const usernameRef = useRef(null);
+    const addressRef = useRef(null);
+
+    const [step, setStep] = useState(1);
 
     const handleInput =(name, value) => {
         setFormData({
@@ -68,36 +72,91 @@ export default function Form(){
         }
     }
 
-    return(
-        <form onSubmit={handleSubmit} noValidate>
-            <h1>Form app</h1>
-            <div className='email-container'>
-                <label htmlFor='email' className='ml-2'>email</label>
-                <Input
-                    type='email'
-                    name='email'
-                    id='email'
-                    ref={emailRef}
-                    handleInput={handleInput}
-                    handleValidation={handleValidation}
-                    className='ml-2'
-                />
-                <p className='form-err pl-4 h-2 ml-1'>{formError.email}</p>
+    if (step ===1) {
+        return(
+            <div>
+                <form onSubmit={handleSubmit} noValidate>
+                    <h1>Form app Step-1</h1>
+                    <div className='email-container'>
+                        <label htmlFor='email' className='ml-2'>email</label>
+                        <Input
+                            type='email'
+                            name='email'
+                            id='email'
+                            ref={emailRef}
+                            handleInput={handleInput}
+                            handleValidation={handleValidation}
+                            className='ml-2'
+                            key={1}
+                        />
+                        <p className='form-err pl-4 h-2 ml-1'>{formError.email}</p>
+                    </div>
+                    <div className='password-container'>
+                        <label htmlFor='password'>password</label>
+                        <Input
+                            type='password'
+                            name='password'
+                            id='password'
+                            ref={passwordRef}
+                            handleInput={handleInput}
+                            handleValidation={handleValidation}
+                            className='ml-2'
+                            key={2}
+                        />
+                        <p className='form-err pl-6 h-2 ml-1'>{formError.password}</p>
+                    </div>
+                    <input type='submit' value='Submit'/>
+                </form>
+                <button onClick={()=> setStep(step+1)}>Next</button>
             </div>
-            <div className='password-container'>
-                <label htmlFor='password'>password</label>
-                <Input
-                    type='password'
-                    name='password'
-                    id='password'
-                    ref={passwordRef}
-                    handleInput={handleInput}
-                    handleValidation={handleValidation}
-                    className='ml-2'
-                />
-                <p className='form-err pl-6 h-2 ml-1'>{formError.password}</p>
+        )
+    } else if (step ===2) {
+        return (
+            <div>
+                <form onSubmit={handleSubmit} noValidate>
+                    <h1>Form app Step-2</h1>
+                    <div className='email-container'>
+                        <label htmlFor='username' className='ml-2'>Username</label>
+                        <Input
+                            type='text'
+                            name='username'
+                            id='username'
+                            ref={usernameRef}
+                            handleInput={handleInput}
+                            handleValidation={handleValidation}
+                            className='ml-2'
+                            key={3}
+                        />
+                    </div>
+                    <input type='submit' value='Submit'/>
+                </form>
+                <button onClick={()=> setStep(step-1)}>Previous</button>
+                <button onClick={()=> setStep(step+1)}>Next</button>
             </div>
-            <input type='submit' value='Submit'/>
-        </form>
-    )
+        )
+    } else if (step ===3) {
+        return (
+            <div>
+                <form onSubmit={handleSubmit} noValidate>
+                    <h1>Form app Step-3</h1>
+                    <div className='email-container'>
+                        <label htmlFor='address' className='ml-2'>Address</label>
+                        <Input
+                            type='text'
+                            name='address'
+                            id='address'
+                            ref={addressRef}
+                            handleInput={handleInput}
+                            handleValidation={handleValidation}
+                            className='ml-2'
+                            key={4}
+                        />
+                    </div>
+                    <input type='submit' value='Submit'/>
+                </form>
+                <button onClick={()=> setStep(step-1)}>Previous</button>
+            </div>
+        )
+    }
+
 };
